dc705c86fc6d0ec3c8532d20328205d7fffe0812
yehudit609 <37326058609@mby.co.il>
2024-07-08 18:09:15 +0300

    update the project

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const authController = require("../controllers/authController")
-
-
-router. post("/login",authController.login)
-router.post("/register",authController.register)
-
-module.exports = router
